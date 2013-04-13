401b96eb5d724fd84c561023c5ca8ebc470c9488
Timo Bingmann <tbgit@panthema.net>
2013-04-13 10:05:59 +0200

    Fixing linecount tool to distinguish between char count with and without newlines.

--- tools/linecount.c
+++ tools/linecount.c
@@ -1,6 +1,6 @@
 /*
  * Simple C program to count the line lengths in an input stream.
- * 2013-04-12 Timo Bingmann <tb@panthema.net>
+ * 2013-04-13 Timo Bingmann <tb@panthema.net>
  *
  * Compile: gcc -O3 -W -Wall linecount.c -o linecount
  * 
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 int main()
@@ -16,51 +17,83 @@ int main()
     typedef unsigned long long ull_type;
 
     int c;
-    ull_type count_lines = 0, count_chars = 0, length_sum = 0;
-    unsigned int i;
+    ull_type line_count = 0, char_count = 0, length_sum = 0;
+    unsigned int i, char_used;
     ull_type ln, length;
 
-    const unsigned int maxmark = 1024*1024;
+    ull_type charmark[256];
 
-    ull_type* mark = malloc( (maxmark+1) * sizeof(ull_type) );
-    memset(mark, 0, sizeof(mark));
+    const unsigned int max_linemark = 1024*1024;
+
+    // clear charmark
+    memset(charmark, 0, sizeof(charmark));
+
+    // allocate and clear linemark
+    ull_type* linemark = malloc( (max_linemark+1) * sizeof(ull_type) );
+    memset(linemark, 0, sizeof(linemark));
 
     ln = 0; /* line start */
     while ( (c = getc(stdin)) != EOF )
     {
         if (c == '\n') {
-            length = count_chars - ln;
+            length = char_count - ln;
             length_sum += length;
-            ++count_lines;
+            ++line_count;
             
-            if (length < maxmark)
-                mark[length]++;
+            if (length < max_linemark)
+                linemark[length]++;
             else
-                mark[maxmark]++;
+                linemark[max_linemark]++;
 
-            ln = count_chars+1; /* next line start */
+            ln = char_count+1; /* next line start */
         }
-        ++count_chars;
+        ++char_count;
+        ++charmark[c];
     }
     /* count last line */
-    if (ln <= count_chars) {
-        length = count_chars - ln;
+    if (ln <= char_count) {
+        length = char_count - ln;
         length_sum += length;
-        ++count_lines;
+        ++line_count;
             
-        if (length < maxmark)
-            mark[length]++;
+        if (length < max_linemark)
+            linemark[length]++;
         else
-            mark[maxmark]++;
+            linemark[max_linemark]++;
     }
 
-    printf("Total: %llu lines in in %llu bytes, average length %.6f.\n",
-           count_lines, count_chars, length_sum / (double)count_lines);
-
-    for (i = 0; i < maxmark; ++i)
+    /* count used characters */
+    char_used = 0;
+    for (i = 0; i < 256; ++i)
     {
-        if (mark[i] == 0) continue;
-        printf("%u\t%llu\n", i, mark[i]);
+        if (charmark[i] == 0) continue;
+        ++char_used;
+    }
+
+    printf("Total: %llu lines in %llu bytes, alphabet %u, average line length %.6f.\n",
+           line_count, char_count, char_used, (length_sum + line_count) / (double)line_count);
+
+    printf("Excluding newline: %llu characters, alphabet %u, average line length %.6f.\n",
+           length_sum, char_used - (charmark['\n'] ? 1 : 0), length_sum / (double)line_count);
+
+    for (i = 0; i < 256; ++i)
+    {
+        if (charmark[i] == 0) continue;
+
+        if (isprint(i)) {
+            printf("char['%c'] = %llu\n", (char)i, charmark[i]);
+        }
+        else {
+            printf("char[%u] = %llu\n", i, charmark[i]);
+        }
+    }
+
+    printf("Line Length Distribution:\n");
+
+    for (i = 0; i < max_linemark; ++i)
+    {
+        if (linemark[i] == 0) continue;
+        printf("%u\t%llu\n", i, linemark[i]);
     }
 
     return 0;
