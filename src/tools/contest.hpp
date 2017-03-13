54d54cb2717a6675c05ddf0c4f9456e69038681b
Timo Bingmann <tbgit@panthema.net>
2017-03-13 07:32:57 +0100

    contest: add __attribute__((unused))

--- src/tools/contest.hpp
+++ src/tools/contest.hpp
@@ -103,12 +103,14 @@ public:
     virtual bool is_parallel() const { return false; }
 };
 
-#define PSS_CONTESTANT(func, algoname, desc)                           \
-    static const class Contestant* _Contestant_ ## func ## _register = \
+#define PSS_CONTESTANT(func, algoname, desc)                         \
+    static const class Contestant* _Contestant_ ## func ## _register \
+    __attribute__((unused)) =                                        \
             new Contestant_UCArray(NULL, func, algoname, desc);
 
-#define PSS_CONTESTANT_PREPARE(pfunc, func, algoname, desc)            \
-    static const class Contestant* _Contestant_ ## func ## _register = \
+#define PSS_CONTESTANT_PREPARE(pfunc, func, algoname, desc)          \
+    static const class Contestant* _Contestant_ ## func ## _register \
+    __attribute__((unused)) =                                        \
             new Contestant_UCArray(pfunc, func, algoname, desc);
 
 class Contestant_UCArray_Parallel : public Contestant_UCArray
@@ -126,12 +128,14 @@ public:
     virtual bool is_parallel() const { return true; }
 };
 
-#define PSS_CONTESTANT_PARALLEL(func, algoname, desc)                  \
-    static const class Contestant* _Contestant_ ## func ## _register = \
+#define PSS_CONTESTANT_PARALLEL(func, algoname, desc)                    \
+    static const class Contestant* _Contestant_ ## func ## _register     \
+    __attribute__((unused)) =                                            \
             new Contestant_UCArray_Parallel(NULL, func, algoname, desc);
 
-#define PSS_CONTESTANT_PARALLEL_PREPARE(pfunc, func, algoname, desc)   \
-    static const class Contestant* _Contestant_ ## func ## _register = \
+#define PSS_CONTESTANT_PARALLEL_PREPARE(pfunc, func, algoname, desc)      \
+    static const class Contestant* _Contestant_ ## func ## _register      \
+    __attribute__((unused)) =                                             \
             new Contestant_UCArray_Parallel(pfunc, func, algoname, desc);
 
 #else // !PSS_CONTEST
